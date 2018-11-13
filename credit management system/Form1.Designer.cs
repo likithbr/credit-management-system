f6f6dc03f7ad9166bf144e304874bf6516729066
likigowda12 <likigowda12@gmail.com>
2018-11-13 15:40:57 +0530

    random change

--- credit management system/Form1.Designer.cs
+++ credit management system/Form1.Designer.cs
@@ -58,7 +58,7 @@
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(211, 214);
+            this.button2.Location = new System.Drawing.Point(229, 214);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(111, 58);
             this.button2.TabIndex = 2;
